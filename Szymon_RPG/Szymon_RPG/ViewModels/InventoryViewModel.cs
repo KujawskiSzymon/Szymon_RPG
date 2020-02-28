68f0f58d6f6fc372e88d1bb7b1d043f76c4f2847
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-02-28 15:12:07 +0100

    Inventory - show no item msg when no items are in eq

---
+++ Szymon_RPG/Szymon_RPG/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Szymon_RPG.Models;
+
+namespace Szymon_RPG.ViewModels
+{
+   public class InventoryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool anyItems;
+        public Boolean AnyItems
+        {
+            get
+            {
+                return anyItems;
+            }
+            set
+            {
+                anyItems = value;
+                OnPropertyChanged("AnyItems");
+            }
+        }
+
+        private bool msganyItems;
+        public Boolean MsgAnyItems
+        {
+            get
+            {
+                return msganyItems;
+            }
+            set
+            {
+                msganyItems = value;
+                OnPropertyChanged("MsgAnyItems");
+            }
+        }
+
+        private List<Item> items;
+        public List<Item> Items
+        {
+            get
+            {
+                return items;
+            }
+            set
+            {
+                items = value;
+                OnPropertyChanged("Items");
+            }
+        }
+       public InventoryViewModel()
+        {
+            Items = Constants.Hero.inventory.items;
+            if (items == null)
+            {
+                AnyItems = false;
+                MsgAnyItems = true;
+            }
+            else {
+                AnyItems = Items?.Count == 0 ? false : true;
+                MsgAnyItems = !AnyItems;
+            }
+            
+
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
